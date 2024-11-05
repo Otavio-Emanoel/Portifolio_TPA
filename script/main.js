512c9b64f9901c678ba7c6abb58642067780f8ff
Otavio Emanoel <otavioemanuel6b@gmail.com>
2024-11-05 11:42:34 -0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -32,9 +32,45 @@ exercicio_1_bim1_close.addEventListener("click", () => {
     exercicio_1_bim1.style.opacity = "0"
     setTimeout(() => {
         exercicio_1_bim1.style.display = "none"
+        document.getElementById("bim1_ex1_res").innerHTML = ""
     }, 1000);
 })
 exercicio_1_bim1_open.addEventListener("click", () => {
     exercicio_1_bim1.style.opacity = "1"
     exercicio_1_bim1.style.display = "flex"
 })
+
+//Exercicio 2 - Bimestre 1
+
+const exercicio_2_bim1 = document.getElementById("modal_bim1_ex2")
+const exercicio_2_bim1_open = document.getElementById("atividade_2_bim_1")
+const exercicio_2_bim1_start = document.getElementById("bim1_ex2_see")
+const exercicio_2_bim1_close = document.getElementById("bim1_ex2_close")
+
+exercicio_2_bim1_close.addEventListener("click", () => {
+    exercicio_2_bim1.style.opacity = "0"
+    setTimeout(() => {
+        exercicio_2_bim1.style.display = "none"
+        document.getElementById("bim1_ex2_res").innerHTML = ""
+    }, 1000);
+});
+
+exercicio_2_bim1_open.addEventListener("click", () => {
+    exercicio_2_bim1.style.opacity = "1"
+    exercicio_2_bim1.style.display = "flex"
+});
+
+exercicio_2_bim1_start.addEventListener('click', () => {
+    let num1 = Number(document.getElementById("bim1_ex2_input1").value);
+    let num2 = Number(document.getElementById("bim1_ex2_input2").value);
+    let num3 = Number(document.getElementById("bim1_ex2_input3").value);
+    let soma = num1 + num2 + num3;
+    let subtração = num1 - num2 - num3;
+    let mult = num1 * num2 * num3;
+    let media = soma / 3;
+    document.getElementById("bim1_ex2_res").innerHTML = `
+    Soma: ${soma} <br>
+    Subtração: ${subtração} <br>
+    Multiplicação: ${mult} <br>
+    Média: ${media}`
+});
